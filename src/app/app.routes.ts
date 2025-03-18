b05611620567b125245c5594a8d9763c90a20afe
kawanmiguel2003 <miguel.kawan2003@gmail.com>
2025-03-18 18:20:43 -0300

    Atualizado .gitignore para ignorar pastas desnecessárias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProfessorAdicionarComponent } from './professor/professor-adicionar/professor-adicionar.component';
+import { ProfessorListarComponent } from './professor/professor-listar/professor-listar.component';
+import { ProfessorEditarComponent } from './professor/professor-editar/professor-editar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'professor/adicionar', component: ProfessorAdicionarComponent },
+    { path: 'professor/listar', component: ProfessorListarComponent },
+    { path: 'professor/editar/:id', component: ProfessorEditarComponent }
+];
